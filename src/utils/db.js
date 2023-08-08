196a9f6b62920831b30f2e397783db7992b1476a
SouvikJoy <souviksahajoy@gmail.com>
2023-08-08 20:25:34 +0600

    Second Commit

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGD);
+    await mongoose.connect(process.env.MONGO_DB);
   } catch (error) {
     throw new Error("Connection failed!");
   }
